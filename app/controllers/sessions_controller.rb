15c49be93aa385cced6b9209d1cf024ba0b5b333
nov <nov@matake.jp>
2016-08-11 19:30:32 +0900

    fixfix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,26 +6,33 @@ class SessionsController < ApplicationController
     if params[:oauth]
       redirect_to current_client.authorization_uri(
         response_type: :token,
-        scope: [:email, :profile]
+        scope: [:email, :profile],
+        state: (session[:state] = SecureRandom.hex(16))
       )
     else
       redirect_to current_client.authorization_uri(
         response_type: [:token, :id_token],
-        scope: [:openid, :email, :profile]
+        scope: [:openid, :email, :profile],
+        state: (session[:state] = SecureRandom.hex(16)),
+        nonce: (session[:nonce] = SecureRandom.hex(16))
       )
     end
   end
 
   def create
-    access_token = current_client.tokenize params[:access_token]
-    account = if params[:id_token].present?
-      Account.authenticate_by_id_token params[:id_token], current_client.jwks
+    if session.delete(:state) == params[:state]
+      access_token = current_client.tokenize params[:access_token]
+      account = if params[:id_token].present?
+        Account.authenticate_by_id_token params[:id_token], current_client, session.delete(:nonce)
+      else
+        Account.authenticate_by_access_token access_token
+      end
+      account.import! access_token.userinfo! if account.new_record?
+      authenticate account
+      redirect_to root_url
     else
-      Account.authenticate_by_access_token access_token
+      raise 'CSRF Attack Detected'
     end
-    account.import! access_token.userinfo! if account.new_record?
-    authenticate account
-    redirect_to root_url
   end
 
   def destroy
--- app/models/account.rb
+++ app/models/account.rb
@@ -6,9 +6,18 @@ class Account < ApplicationRecord
   end
 
   class << self
-    def authenticate_by_id_token(id_token_str, jwks)
-      id_token = JSON::JWT.decode id_token_str, jwks
-      find_or_initialize_by(identifier: id_token[:sub])
+    def authenticate_by_id_token(id_token_str, client, nonce)
+      id_token = JSON::JWT.decode id_token_str, client.jwks
+      if (
+        id_token[:iss] == client.issuer &&
+        id_token[:aud] == client.identifier &&
+        id_token[:nonce] == nonce &&
+        Time.at(id_token[:exp]) >= Time.now
+      )
+        find_or_initialize_by(identifier: id_token[:sub])
+      else
+        raise 'ID Token Invalid'
+      end
     end
 
     def authenticate_by_access_token(access_token)
