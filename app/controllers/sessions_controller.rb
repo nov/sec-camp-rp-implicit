eeb2e441e16285a7ba514772e776a8dec41d82a5
nov <nov@matake.jp>
2016-08-11 19:56:59 +0900

    validate state filter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :validate_state!, only: :create
+
   def show
   end
 
@@ -21,19 +23,15 @@ class SessionsController < ApplicationController
 
   def create
     raise params[:error] if params[:error].present?
-    if session.delete(:state) == params[:state]
-      access_token = current_client.tokenize params[:access_token]
-      account = if params[:id_token].present?
-        Account.authenticate_by_id_token params[:id_token], current_client, session.delete(:nonce)
-      else
-        Account.authenticate_by_access_token access_token
-      end
-      account.import! access_token.userinfo! if account.new_record?
-      authenticate account
-      redirect_to root_url
+    access_token = current_client.tokenize params[:access_token]
+    account = if params[:id_token].present?
+      Account.authenticate_by_id_token params[:id_token], current_client, session.delete(:nonce)
     else
-      raise 'CSRF Attack Detected'
+      Account.authenticate_by_access_token access_token
     end
+    account.import! access_token.userinfo! if account.new_record?
+    authenticate account
+    redirect_to root_url
   end
 
   def destroy
@@ -45,5 +43,11 @@ class SessionsController < ApplicationController
 
   def current_client
     @client ||= Client.first
+  end
+
+  def validate_state!
+    unless session.delete(:state) == params[:state]
+      raise 'CSRF Attack Detected'
+    end
   end
 end
